19e0a582735e497bbd811760954507fb4426c5d2
Tom Yaxley <tommitytom@gmail.com>
2016-09-19 12:51:38 +1000

    Removed unnecessary globals

--- src/Browser.js
+++ src/Browser.js
@@ -2,10 +2,6 @@ import TetrisApp from './TetrisApp';
 import TetrisCanvasRenderer from './TetrisCanvasRenderer';
 import KeyboardController from './KeyboardController';
 
-global.TetrisApp = TetrisApp;
-global.TetrisCanvasRenderer = TetrisCanvasRenderer;
-global.KeyboardController = KeyboardController;
-
 window.onload = () => { 
 	let renderer = new TetrisCanvasRenderer('tetris', 12, 24),
 		controller = new KeyboardController('tetris'),
