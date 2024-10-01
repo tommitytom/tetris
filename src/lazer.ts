2fa4dd49100d9f0ab7079d42ce1386e742339311
tommitytom <git@tommitytom.co.uk>
2024-10-01 14:13:23 +1000

    Add midi note sending

--- src/lazer.ts
+++ src/lazer.ts
@@ -1,20 +1,33 @@
+import Audic from 'audic';
+import midi from 'midi';
+import { Client } from 'node-osc';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+import GamepadController from './GamepadController';
+import NodeKeyboardController from './NodeKeyboardController';
 import Tetris from './Tetris';
 import TetrisIldaRenderer from './TetrisIldaRenderer';
 
-import Audic from 'audic';
-import { Client } from 'node-osc';
-import GamepadController from './GamepadController';
-import NodeKeyboardController from './NodeKeyboardController';
+const ENABLE_SFX = false;
+const ENABLE_BGM = false;
+const ENABLE_MIDI = false;
 
-import { fileURLToPath } from 'url';
-import * as path from 'path';
+const NOTE_ROTATE = 55;
+const NOTE_LAND = 56;
+const NOTE_CLEAR = 57;
+
+const osc = new Client('192.168.0.130', 3333);
+
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
+const bgm = new Audic(`${__dirname}/../assets/TEST.ogg`);
+const sfx1 = new Audic(`${__dirname}/../assets/SF1.wav`);
+const sfx2 = new Audic(`${__dirname}/../assets/SF2.wav`);
 
 const renderer = new TetrisIldaRenderer(12, 24);
 const tetris = new Tetris(renderer.gridSize.w, renderer.gridSize.h);
 renderer.start(tetris);
-
 
 function handleControllerEvent(name) {
     switch (name) {
@@ -36,14 +49,13 @@ const keyboardController = new NodeKeyboardController();
 gamepadController.onEvent(handleControllerEvent);
 keyboardController.onEvent(handleControllerEvent);
 
-const osc = new Client('192.168.0.130', 3333);
+let midiOutput: midi.Output;
 
-const ENABLE_SFX = false;
-const ENABLE_BGM = false;
-
-const bgm = new Audic(`${__dirname}/../assets/TEST.ogg`);
-const sfx1 = new Audic(`${__dirname}/../assets/SF1.wav`);
-const sfx2 = new Audic(`${__dirname}/../assets/SF2.wav`);
+if (ENABLE_MIDI) {
+    midiOutput = new midi.Output();
+    console.log(`Opening midi port ${midiOutput.getPortName(0)}`);
+    midiOutput.openPort(0);
+}
 
 tetris.on('begin', () =>{
     console.log('BEGIN');
@@ -62,10 +74,17 @@ tetris.on('rotate', () =>{
         sfx1.play();
     }
 
+    if (midiOutput) {
+        midiOutput.sendMessage([144,NOTE_ROTATE,127])
+    }
+
     //osc.send('/SCENE/0', 1);
 });
 
 tetris.on('land', () =>{
+    if (midiOutput) {
+        midiOutput.sendMessage([144,NOTE_LAND,127])
+    }
     //osc.send('/SCENE/0', 1);
 });
 
@@ -76,6 +95,10 @@ tetris.on('death', () =>{
 tetris.on('removeBegin', (amount: number) => {
     if (ENABLE_SFX) {
         sfx2.play();
+    }
+
+    if (midiOutput) {
+        midiOutput.sendMessage([144,NOTE_CLEAR,127])
     }
     
     osc.send('/SCENE/2', 1);
